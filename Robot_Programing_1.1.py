d57ceca5b2a17f9d7e56ee5958baca9e509cb8b5
Roctrico <fortnight2213@outlook.com>
2026-01-28 11:27:10 +0000

    Altered code to allow black stript to be stopped mid search to allow white script to run

--- Robot_Programing_1.1.py
+++ Robot_Programing_1.1.py
@@ -10,53 +10,88 @@ from hub import port
 while True:
 #Movement commands for when sesor reads white/path
     async def main ():
-        if color_sensor.color(port.E) is color.WHITE:
-            motor.run_for_degrees (port.D, -150, 200)
-            motor.run_for_degrees (port.C, 150,200)
+        async def white ():
+            if color_sensor.color(port.E) is color.WHITE:
+                motor.run_for_degrees (port.D, -150, 200)
+                motor.run_for_degrees (port.C, 150,200)
+        runloop.run(white()) 
 #Search command for when sensor reads black/Road
-        if color_sensor.color(port.E) is color.BLACK:
-            motor.run_for_degrees (port.D, 20, 200)
-            await motor.run_for_degrees (port.C, 20, 200)
-            await motor.run_for_degrees (port.D, 0,0)
-            motor.run_for_degrees (port.C, 0, 0)
-            motor.run_for_degrees (port.D, -40, 200)
-            await motor.run_for_degrees (port.C, -40, 200)
-            await motor.run_for_degrees (port.D, 0,0)
-            motor.run_for_degrees (port.C, 0, 0)
-            motor.run_for_degrees (port.D, 60, 200)
-            await motor.run_for_degrees (port.C, 60, 200)
-            await motor.run_for_degrees (port.D, 0,0)
-            motor.run_for_degrees (port.C, 0, 0)
-            motor.run_for_degrees (port.D, -80, 200)
-            await motor.run_for_degrees (port.C, -80, 200)
-            await motor.run_for_degrees (port.D, 0,0)
-            motor.run_for_degrees (port.C, 0, 0)
-            motor.run_for_degrees (port.D, 100, 200)
-            await motor.run_for_degrees (port.C, 100, 200)
-            await motor.run_for_degrees (port.D, 0,0)
-            motor.run_for_degrees (port.C, 0, 0)
-            motor.run_for_degrees (port.D, -120, 200)
-            await motor.run_for_degrees (port.C, -120, 200)            
-            motor.run_for_degrees (port.D, 140, 200)
-            await motor.run_for_degrees (port.C, 140, 200)
-            await motor.run_for_degrees (port.D, 0,0)
-            motor.run_for_degrees (port.C, 0, 0)
-            motor.run_for_degrees (port.D, -160, 200)
-            await motor.run_for_degrees (port.C, -160, 200)
-            await motor.run_for_degrees (port.D, 0,0)
-            motor.run_for_degrees (port.C, 0, 0)
-            motor.run_for_degrees (port.D, 180, 200)
-            await motor.run_for_degrees (port.C, 180, 200)
-            await motor.run_for_degrees (port.D, 0,0)
-            motor.run_for_degrees (port.C, 0, 0)
-            motor.run_for_degrees (port.D, -200, 200)
-            await motor.run_for_degrees (port.C, -200, 200)
-            await motor.run_for_degrees (port.D, 0,0)
-            motor.run_for_degrees (port.C, 0, 0)
-            motor.run_for_degrees (port.D, 220, 200)
-            await motor.run_for_degrees (port.C, 220, 200)
-            await motor.run_for_degrees (port.D, 0,0)
-            motor.run_for_degrees (port.C, 0, 0)
-            motor.run_for_degrees (port.D, -240, 200)
-            await motor.run_for_degrees (port.C, -240, 200)
+        async def black ():
+          if color_sensor.color(port.E) is color.BLACK:
+                motor.run_for_degrees (port.D, 20, 200)
+                await motor.run_for_degrees (port.C, 20, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)
+                motor.run_for_degrees (port.D, -40, 200)
+                await motor.run_for_degrees (port.C, -40, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)
+                motor.run_for_degrees (port.D, 60, 200)
+                await motor.run_for_degrees (port.C, 60, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)
+                motor.run_for_degrees (port.D, -80, 200)
+                await motor.run_for_degrees (port.C, -80, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)
+                motor.run_for_degrees (port.D, 100, 200)
+                await motor.run_for_degrees (port.C, 100, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)
+                motor.run_for_degrees (port.D, -120, 200)
+                await motor.run_for_degrees (port.C, -120, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)            
+                motor.run_for_degrees (port.D, 140, 200)
+                await motor.run_for_degrees (port.C, 140, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)
+                motor.run_for_degrees (port.D, -160, 200)
+                await motor.run_for_degrees (port.C, -160, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)
+                motor.run_for_degrees (port.D, 180, 200)
+                await motor.run_for_degrees (port.C, 180, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)
+                motor.run_for_degrees (port.D, -200, 200)
+                await motor.run_for_degrees (port.C, -200, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)
+                motor.run_for_degrees (port.D, 220, 200)
+                await motor.run_for_degrees (port.C, 220, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+                await motor.run_for_degrees (port.D, 0,0)
+                motor.run_for_degrees (port.C, 0, 0)
+                motor.run_for_degrees (port.D, -240, 200)
+                await motor.run_for_degrees (port.C, -240, 200)
+                if color_sensor.color(port.E) is color.WHITE:
+                    return
+        runloop.run(black())
     runloop.run(main())
+
+
+
+        
+
